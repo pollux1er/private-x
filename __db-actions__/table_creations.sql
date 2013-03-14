9aa85d898605b49278de379f71669ee95e4549dc
atspinto <spinto2008@live.fr>
2013-03-14 22:58:05 +0100

    mm

--- __db-actions__/table_creations.sql
+++ __db-actions__/table_creations.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Mer 13 Mars 2013 à 20:49
+-- Généré le : Jeu 14 Mars 2013 à 21:45
 -- Version du serveur: 5.1.36
 -- Version de PHP: 5.3.0
 
@@ -12,6 +12,29 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Base de données: `smsforever`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `country`
+--
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `country` varchar(3) NOT NULL,
+  `ind_num` varchar(6) NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `country`
+--
+
+INSERT INTO `country` (`id`, `country`, `ind_num`, `status`) VALUES
+(1, 'CMR', '+237', '1'),
+(2, 'CIV', '+225', '1'),
+(3, 'GHA', '+233', '1');
 
 -- --------------------------------------------------------
 
@@ -47,10 +70,10 @@ CREATE TABLE IF NOT EXISTS `users` (
   `passwd` varchar(50) NOT NULL,
   `num_tel` varchar(15) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `status` enum('0','1') NOT NULL,
+  `status` enum('0','1') NOT NULL DEFAULT '0',
   `country` varchar(3) NOT NULL,
   `reseau_tel` varchar(3) NOT NULL,
-  `registered` enum('0','1') NOT NULL,
+  `registered` enum('0','1') NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
